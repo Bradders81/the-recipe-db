0b88c931e099662f14fbefe68e9d249fc339bd71
Bradders81 <p-bradley@live.co.uk>
2021-05-31 18:32:02 +0000

    Add hero image, call out text, call to action btn

--- app.py
+++ app.py
@@ -19,8 +19,13 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/get_recipes")
-def get_recipes():
+@app.route("/home")
+def home_page():
+    return render_template("index.html")
+
+
+@app.route("/recipes")
+def recipes():
     recipes = mongo.db.recipes.find()
     return render_template("recipes.html", recipes=recipes)
 
