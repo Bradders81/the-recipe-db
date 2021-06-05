673414f6803a6e70087773d428186f88311c9c25
Bradders81 <p-bradley@live.co.uk>
2021-06-05 16:10:47 +0000

    Update sign up & login button style

--- app.py
+++ app.py
@@ -36,6 +36,11 @@ def login():
     return render_template("login.html")
 
 
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
